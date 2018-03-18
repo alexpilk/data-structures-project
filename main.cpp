a0bed23cded04476d8b9e68dedac1977dfe7b489
Alex <alex.pilkevych@gmail.com>
2018-03-18 14:44:39 +0200

    Heap, array and list measurements.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@
 #include "Heap.h"
 #include "utils.h"
 #include "chrono"
+#include "random"
 
 using namespace std;
-void test_list(){
+
+void test_list() {
     cout << "Testing linked list" << endl;
     List my_list = List();
     my_list.addFirst(1);
@@ -28,7 +30,7 @@ void test_list(){
     cout << my_list.getSize() << endl;
 }
 
-void test_dynamic_array(){
+void test_dynamic_array() {
     cout << "Testing dynamic array" << endl;
     DynamicArray my_array = DynamicArray();
     my_array.print();
@@ -47,11 +49,11 @@ void test_dynamic_array(){
     my_array.print();
 };
 
-void test_heap(){
+void test_heap() {
     cout << "Testing heap" << endl;
     int base_array[] = {2, 9, 4, 4, 2, 6, 4, 5, 5, 6, 7, 8, 1, 9, 5};
     int base_array_size = getArraySize(base_array);
-    auto* input_array = new List(base_array, base_array_size);
+    auto *input_array = new List(base_array, base_array_size);
     Heap my_heap = Heap(input_array);
     my_heap.insert(80);
     my_heap.insert(800);
@@ -61,20 +63,80 @@ void test_heap(){
     my_heap.print();
 }
 
-template<class T, typename ReturnType, typename... Args>
-double measure(T* my_list, ReturnType (T::*func)(Args...), Args... args){
-    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
-    (my_list->*func)(args...);
-    chrono::steady_clock::time_point end = chrono::steady_clock::now();
-    return chrono::duration_cast<chrono::microseconds>(end - begin).count();
+
+void list_vs_array(int repetitions) {
+    long long int time_taken;
+    int ARRAY_SIZE = 100000;
+    int bottom_limit = 0;
+    int top_limit = 1;
+    int value_to_add = ARRAY_SIZE / 2;
+    int insert_index = ARRAY_SIZE / 2;
+    int start_array[ARRAY_SIZE];
+    fillWithRandomValues(start_array, ARRAY_SIZE, 0, 1);
+    cout << "Add F\tAdd L\tIns\tRem F\tRem L\tRem IND" << endl;
+
+    for (int i = 0; i < repetitions; ++i) {
+
+//         List
+        auto *my_list = new List(start_array, ARRAY_SIZE);
+        time_taken = measure(my_list, my_list->addFirst, value_to_add);
+        time_taken = measure(my_list, my_list->addLast, value_to_add);
+        time_taken = measure(my_list, my_list->insert, value_to_add, insert_index);
+
+        time_taken = measure(my_list, my_list->removeFirst);
+        time_taken = measure(my_list, my_list->removeLast);
+        time_taken = measure(my_list, my_list->removeOnIndex, insert_index);
+
+        // Array
+//    auto* my_array = new DynamicArray(start_array, ARRAY_SIZE);
+//        time_taken = measure(my_array, my_array->addFirst, value_to_add);
+//        time_taken = measure(my_array, my_array->addLast, value_to_add);
+//        time_taken = measure(my_array, my_array->insert, value_to_add, ARRAY_SIZE/2);
+//
+//        time_taken = measure(my_array, my_array->removeFirst);
+//        time_taken = measure(my_array, my_array->removeLast);
+//        time_taken = measure(my_array, my_array->removeOnIndex, ARRAY_SIZE/2);
+        cout << endl;
+    }
 }
 
-int main(){
-    int base_array[] = {2, 9, 4, 4, 2, 6, 4, 5, 5, 6, 7, 8, 1, 9, 5};
-    auto* my_list = new List(base_array, getArraySize(base_array));
-    my_list->print();
-    double time = measure(my_list, my_list->getSize);
-    my_list->print();
-    cout << time << endl;
+
+void heap_measurements(int repetitions) {
+    long long int time_taken;
+    int ARRAY_SIZE = 50000;
+    int bottom_limit = 0;
+    int top_limit = ARRAY_SIZE;
+    int mid_value = ARRAY_SIZE / 2;
+    int insert_index = ARRAY_SIZE / 2;
+    int start_array[ARRAY_SIZE];
+    fillWithRandomValues(start_array, ARRAY_SIZE, 0, 1);
+    cout << "AIns min\tIns max\tIns middle\tExtract" << endl;
+    auto *input_array = new DynamicArray(start_array, ARRAY_SIZE);
+    auto *my_heap = new Heap(input_array);
+
+    for (int i = 0; i < repetitions; ++i) {
+        time_taken = measure(my_heap, my_heap->insert, bottom_limit);
+        time_taken = measure(my_heap, my_heap->insert, top_limit);
+        time_taken = measure(my_heap, my_heap->insert, mid_value);
+        time_taken = measure(my_heap, my_heap->extract);
+        cout << endl;
+    }
+
+    cout << "LIns min\tIns max\tIns middle\tExtract" << endl;
+    auto *input_list = new List(start_array, ARRAY_SIZE);
+    auto *my_heap_list = new Heap(input_list);
+
+    for (int i = 0; i < repetitions; ++i) {
+        time_taken = measure(my_heap_list, my_heap_list->insert, bottom_limit);
+        time_taken = measure(my_heap_list, my_heap_list->insert, top_limit);
+        time_taken = measure(my_heap_list, my_heap_list->insert, mid_value);
+        time_taken = measure(my_heap_list, my_heap_list->extract);
+        cout << endl;
+    }
+}
+
+int main() {
+//    list_vs_array(100);
+    heap_measurements(100);
     return 0;
 }
--- utils.h
+++ utils.h
@@ -1,10 +1,31 @@
 //
 // Created by Drasik on 3/13/2018.
 //
+#include "random"
+#include "chrono"
 
+using namespace std;
 
 template<typename T,int N>
 int getArraySize(T (&input_array)[N]){
     int input_size = sizeof(input_array) / sizeof(input_array[0]);;
     return input_size;
 };
+
+template<class T, typename ReturnType, typename... Args>
+long long int measure(T *my_list, ReturnType (T::*func)(Args...), Args... args) {
+    chrono::high_resolution_clock::time_point begin = chrono::high_resolution_clock::now();
+    (my_list->*func)(args...);
+    chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
+    long long int result = chrono::duration_cast<chrono::nanoseconds>(end - begin).count();
+    cout << result << "\t";
+    return result;
+}
+
+int* fillWithRandomValues(int start_array[], int array_size, int bottom_limit, int top_limit){
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(bottom_limit,top_limit);
+    for (int i = 0; i < array_size; i++)
+        start_array[i] = distribution(generator);
+    return start_array;
+}
